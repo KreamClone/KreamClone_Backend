8b2d3b732c924fd2ca97444b0eca6081b30df990
seongyun <kanteluv0417@gmail.com>
2023-05-12 19:56:30 +0900

    feat(spring) : 물품정보 관련 dto 추가중

--- src/main/java/com/sparta/kreamclone_backend/dto/ProductResponseDto.java
+++ src/main/java/com/sparta/kreamclone_backend/dto/ProductResponseDto.java
@@ -1,4 +1,27 @@
 package com.sparta.kreamclone_backend.dto;
 
+import com.sparta.kreamclone_backend.entity.Product;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class ProductResponseDto {
+    private String productEngNm;
+    private String productKorNm;
+    private Long productNum;
+    private Long productPrice;
+    private List<ProeductSizesDto> proeductSizesDtoList;
+    private List<CategoriesDto> categoriesDtoList;
+    private List<BrandDto> brandDtoList;
+
+    public ProductResponseDto(Product product) {
+        this.productEngNm = product.getProductEngNm();
+        this.productKorNm = product.getProductKorNm();
+        this.productNum = product.getProductNum();
+        this.productPrice = product.getProductPrice();
+    }
 }
